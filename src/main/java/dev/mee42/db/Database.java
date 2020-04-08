8b4d206bc476ca79061ae0f01c69a495f3b315a9
Carson Graham <carson42g@gmail.com>
2020-04-08 19:45:56 -0400

    added key.txt

--- src/main/java/dev/mee42/db/Database.java
+++ src/main/java/dev/mee42/db/Database.java
@@ -2,8 +2,24 @@ package dev.mee42.db;
 
 import org.jdbi.v3.core.Jdbi;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Database {
-    public static Jdbi connection = Jdbi.create("jdbc:mariadb://localhost:3306/planetSim?user=ssh&password=ssh-ssh-ssh");
+    private static String getPassword() {
+        try {
+            return Files.readAllLines(new File("./key.txt").toPath()).get(0).trim();
+        } catch (IOException e) {
+            throw doThrow(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static <E extends Exception> E doThrow(Exception e) throws E {
+        throw (E) e;
+    }
+    private static String password = getPassword();
+
+    public static Jdbi connection = Jdbi.create("jdbc:mariadb://localhost:3306/planetSim?user=ssh&password=" + password);
 }
-
-
